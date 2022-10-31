d7c1968c6da6b612ec0d3791eb892c659abb361a
Matt <maddiepatty53@yahoo.com>
2022-10-31 16:43:30 -0500

    Initial Commit

---
+++ FlyerFinal/FlyerFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyerFinalApp.swift
+//  FlyerFinal
+//
+//  Created by macOS on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct FlyerFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
